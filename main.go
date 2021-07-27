87790d4e6f9ebfcf50930196c2b63b69b5ad27d9
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-27 21:39:15 +0530

    update go code

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+fun main() {
+	port:= flag.String("p", "9000", "port to serve on")
+	directory := flag.String("d", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/Menu1.pdf")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServer(":"+*port, nil))
+}
