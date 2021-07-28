e854440d16698d140b7e0f86b4696d3c4b176aeb
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-28 21:22:08 +0530

    test

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
 
-	err := http.ListenAndServeTLS(":"+*port, "/home/prateek/dev_local/go/certs/pch-csr.cer", "/home/prateek/dev_local/go/src/certs/pch-pkey.pem", nil)
+	err := http.ListenAndServeTLS(":"+*port, "/home/prateek/dev_local/go/certs/pch-csr.cer", "/home/prateek/dev_local/go/certs/pch-pkey.pem", nil)
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 
