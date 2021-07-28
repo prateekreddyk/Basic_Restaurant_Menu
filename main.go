0c61d6630e86fdd8818da5c19c89063d9b5af7eb
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-28 22:38:04 +0530

    test

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
+// package main
+
+// import (
+// 	"flag"
+// 	"log"
+// 	"net/http"
+// )
+
+// func main() {
+// 	//port := flag.String("p", "8443", "port to serve on")
+// 	directory := flag.String("d", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/Menu/", "menu")
+// 	flag.Parse()
+
+// 	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+// 	err := http.ListenAndServeTLS(":8443, "/home/prateek/server.crt", "/home/prateek/server.key", nil)
+
+// 	//log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+
+// 	if err != nil {
+// 		log.Fatal("ListenAndServeTLS: ", err)
+// 	}
+// }
+
 package main
 
 import (
-	"flag"
+	// "fmt"
+	// "io"
 	"log"
 	"net/http"
 )
 
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+	// fmt.Fprintf(w, "This is an example server.\n")
+	// io.WriteString(w, "This is an example server.\n")
+}
+
 func main() {
-	port := flag.String("p", "8443", "port to serve on")
-	directory := flag.String("d", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/Menu/", "menu")
-	flag.Parse()
-
-	http.Handle("/", http.FileServer(http.Dir(*directory)))
-
-	err := http.ListenAndServeTLS(":"+*port, "/home/prateek/server.crt", "/home/prateek/server.key", nil)
-
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
 	if err != nil {
-		log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-		log.Fatal("ListenAndServeTLS: ", err)
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
