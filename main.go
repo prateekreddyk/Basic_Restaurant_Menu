e532fff0dacd3978e14c847cb57d5d24f28d3801
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-28 21:33:38 +0530

    psu

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	port := flag.String("p", "8443", "port to serve on")
+	port := flag.String("p", "9004", "port to serve on")
 	directory := flag.String("d", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/Menu/", "menu")
 	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
 
-	err := http.ListenAndServeTLS(":8443", "/home/prateek/dev_local/go/certs/pch-csr.crt", "/home/prateek/dev_local/go/certs/pch-pkey.pem", nil)
+	err := http.ListenAndServeTLS(":9004", "/home/prateek/dev_local/go/certs/pch-csr.crt", "/home/prateek/dev_local/go/certs/pch-pkey.pem", nil)
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 
