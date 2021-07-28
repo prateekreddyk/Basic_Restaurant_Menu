e0edcdfe94468add56528585757c6c3604e195f8
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-28 17:52:25 +0530

    update

--- GOCODE/Basic_http_image_serve.go
+++ GOCODE/Basic_http_image_serve.go
@@ -7,15 +7,12 @@ import (
 )
 
 func main() {
-	port := flag.String("p", "8443", "port to serve on")
+	port := flag.String("p", "9000", "port to serve on")
 	directory := flag.String("d", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/Menu/", "menu")
 	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
 
-	err := http.ListenAndServeTLS(":"+*port, "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/CERTS/pch-csr.cer", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/CERTS/pch-pkey.pem", nil)
-
-	if err != nil {
-		log.Fatal("ListenAndServeTLS: ", err)
-		log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
