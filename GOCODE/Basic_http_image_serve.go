f0aa3053f21fcb29001968248864649b9cbcd6e4
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-28 17:44:54 +0530

    update

--- GOCODE/Basic_http_image_serve.go
+++ GOCODE/Basic_http_image_serve.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	//port := flag.String("p", "9000", "port to serve on")
+	port := flag.String("p", "8443", "port to serve on")
 	directory := flag.String("d", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/Menu/", "menu")
 	flag.Parse()
 
