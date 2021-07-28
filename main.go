3984ed712b122090728f7a08ac652cc691420f1d
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-28 22:08:53 +0530

    test: g

--- main.go
+++ main.go
@@ -7,17 +7,16 @@ import (
 )
 
 func main() {
-	port := flag.String("p", "9004", "port to serve on")
+	port := flag.String("p", "8443", "port to serve on")
 	directory := flag.String("d", "/home/prateek/dev_local/go/src/Basic_Restaurant_Menu/Menu/", "menu")
 	flag.Parse()
 
 	http.Handle("/", http.FileServer(http.Dir(*directory)))
 
-	err := http.ListenAndServeTLS(":9004", "/home/prateek/dev_local/go/certs/pch-csr.crt", "/home/prateek/dev_local/go/certs/pch-pkey.pem", nil)
-
-	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+	err := http.ListenAndServeTLS(":"+*port, "/home/prateek/dev_local/go/certs/pch-csr.cer", "/home/prateek/dev_local/go/certs/pch-pkey.pem", nil)
 
 	if err != nil {
+		log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 		log.Fatal("ListenAndServeTLS: ", err)
 	}
 }
