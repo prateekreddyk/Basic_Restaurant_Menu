a45fad076dab780b36894ed5010cc39972fe98a3
Prateek Reddy K <prateek.reddyk@gmail.com>
2021-07-28 22:48:22 +0530

    test

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	http.HandleFunc("/hello", HelloServer)
-	err := http.ListenAndServeTLS(":8443", "/home/prateek/server.crt", "/home/prateek/server.key", nil)
+	err := http.ListenAndServeTLS(":8443", "/home/prateek/pch-cert.crt", "/home/prateek/pch-pkey.pem", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
